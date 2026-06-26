135081151f5aca78dba04cdcd28e8b5b0b5d9431
ryansldev <ryanlima1309@gmail.com>
2026-06-26 00:05:04 -0300

    finish `/status` endpoint

--- apps/api/routes/v1/status/index.ts
+++ apps/api/routes/v1/status/index.ts
@@ -4,14 +4,29 @@ import database from "infra/database";
 
 export default async function (fastify: FastifyInstance) {
   fastify.get('/', async () => {
-    const resultDatabaseVersion = await database.execute(sql`SHOW server_version;`)
-    const databaseVersion = resultDatabaseVersion.rows[0]?.server_version ?? null
+    const databaseResult = await database.transaction(async (tx) => {
+      const databaseVersionResult = await tx.execute<{ server_version: string }>(sql`SHOW server_version;`)
+      const databaseMaxConnectionsResult = await tx.execute<{ max_connections: number }>(sql`SHOW max_connections;`)
+      const databaseConnectionsResult = await tx.execute<{ count: number }>(sql`SELECT count(*)::int FROM pg_stat_activity WHERE datname = current_database();`)
+
+      return {
+        version: databaseVersionResult.rows,
+        maxConnections: databaseMaxConnectionsResult.rows,
+        openedConnections: databaseConnectionsResult.rows,
+      };
+    })
+    
+    const databaseVersion = databaseResult.version[0]?.server_version ?? null;
+    const databaseMaxConnections = Number(databaseResult.maxConnections[0]?.max_connections ?? null);
+    const databaseOpenedConnections = databaseResult.openedConnections[0]?.count ?? null;
 
     return {
       dependencies: {
         database: {
-          version: databaseVersion
-        }
+          version: databaseVersion,
+          max_connections: databaseMaxConnections,
+          opened_connections: databaseOpenedConnections,
+        },
       }
     }
   });
