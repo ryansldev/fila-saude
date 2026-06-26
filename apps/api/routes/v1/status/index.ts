79d4bf753156c74b7c0f6dbcef43aa0d9e7399fb
ryansldev <ryanlima1309@gmail.com>
2026-06-25 23:52:18 -0300

    improve `.tsconfig` for api

--- apps/api/routes/v1/status/index.ts
+++ apps/api/routes/v1/status/index.ts
@@ -1,10 +1,11 @@
-import database from "api/infra/database";
 import { sql } from "drizzle-orm";
+import type { FastifyInstance } from "fastify";
+import database from "infra/database";
 
-export default async function (fastify) {
+export default async function (fastify: FastifyInstance) {
   fastify.get('/', async () => {
     const resultDatabaseVersion = await database.execute(sql`SHOW server_version;`)
-    const databaseVersion = resultDatabaseVersion.rows[0].server_version
+    const databaseVersion = resultDatabaseVersion.rows[0]?.server_version ?? null
 
     return {
       dependencies: {
