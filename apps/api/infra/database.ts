842a282aef446155b63e0c178e5f6b83404dbef0
ryansldev <ryanlima1309@gmail.com>
2026-06-25 23:14:40 -0300

    add `api` version return in `/status` endpoint

--- apps/api/infra/database.ts
+++ apps/api/infra/database.ts
@@ -2,6 +2,6 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import env from './env';
 
-const db = drizzle(env.DATABASE_URL);
+const database = drizzle(env.DATABASE_URL);
 
-export default db;
+export default database;
--- apps/api/routes/v1/status/index.ts
+++ apps/api/routes/v1/status/index.ts
@@ -1,5 +1,17 @@
+import database from "api/infra/database";
+import { sql } from "drizzle-orm";
+
 export default async function (fastify) {
   fastify.get('/', async () => {
-    return { status: 'ok' };
+    const resultDatabaseVersion = await database.execute(sql`SHOW server_version;`)
+    const databaseVersion = resultDatabaseVersion.rows[0].server_version
+
+    return {
+      dependencies: {
+        database: {
+          version: databaseVersion
+        }
+      }
+    }
   });
 }
