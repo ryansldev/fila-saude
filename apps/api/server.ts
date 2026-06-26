6b0b74e108680d18814ccbea7d6c123b4c3d44b5
ryansldev <ryanlima1309@gmail.com>
2026-06-26 05:56:22 -0300

    add `vitest`

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -1,19 +1,19 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import autoload from '@fastify/autoload'
-import Fastify from 'fastify'
+import autoload from "@fastify/autoload";
+import Fastify from "fastify";
 
 const app = Fastify({
-  logger: true
-})
+  logger: true,
+});
 
 app.register(autoload, {
-  dir: path.join(path.dirname(fileURLToPath(import.meta.url)), "routes")
-})
+  dir: path.join(path.dirname(fileURLToPath(import.meta.url)), "routes"),
+});
 
 try {
-  await app.listen({ port: 3333 })
+  await app.listen({ port: 3333 });
 } catch (err) {
-  app.log.error(err)
-  process.exit(1)
+  app.log.error(err);
+  process.exit(1);
 }
