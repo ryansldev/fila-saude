79f34f907de38f55574fda9e57a3c72e3759b87f
ryansldev <ryanlima1309@gmail.com>
2026-06-30 20:57:45 -0300

    refactor: add `coderabbit` suggested adjustments

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">The quick brown fox jumps over the lazy dog</h1>
+      <h1 className="text-4xl font-bold">Em desenvolvimento</h1>
     </div>
   );
 }
