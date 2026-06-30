4fd5ac9f774302c3b6854c6d0002ca4efd070c2f
ryansldev <ryanlima1309@gmail.com>
2026-06-30 20:40:30 -0300

    feat: add initial theme
    add initial design theme: colors, typography, shadcn configuration
    
    contribute to #5 theme step

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Work In Progress</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">The quick brown fox jumps over the lazy dog</h1>
     </div>
   );
 }
